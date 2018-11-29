91f85af0fd8fbb5dbc66037f9fe4a27e3ef3b503
Steed16 <dsteedsm@cord.edu>
2018-11-29 08:58:16 -0600

    o[aijd[oiaj

---
+++ BlockWebsites/Globalcs.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlockWebsites
+{
+    public class Globalcs
+    {
+        public void BlockWebsites()
+        {
+            string originalPath = new Uri(HttpContext.Request.Path).OriginalString;
+            MessageBox.Show(originalPath);
+        }
+    }
+}
